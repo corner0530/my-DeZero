9cd016f54e5ee33dcf4ecfaa75d04a7461eedb87
corner0530 <2531yasuaki@gmail.com>
2023-04-06 23:32:54 +0900

    Done step34

---
+++ dezero/functions.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+from dezero.core import Function, Variable
+
+
+class Sin(Function):
+    """sin関数を表すクラス"""
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """順伝播
+
+        Args:
+            x: 入力
+
+        Returns:
+            y: 出力
+        """
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy: Variable) -> Variable:
+        """逆伝播
+
+        Args:
+            gy: 出力側から伝わる微分
+
+        Returns:
+            gx: 入力側に伝わる微分
+        """
+        x = self.inputs[0]
+        gx = gy * cos(x)
+        return gx
+
+
+def sin(x: Variable) -> Variable:
+    """sin関数
+
+    Args:
+        x: 入力
+
+    Returns:
+        y: 出力
+    """
+    return Sin()(x)
+
+
+class Cos(Function):
+    """cos関数を表すクラス"""
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """順伝播
+
+        Args:
+            x: 入力
+
+        Returns:
+            y: 出力
+        """
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy: Variable) -> Variable:
+        """逆伝播
+
+        Args:
+            gy: 出力側から伝わる微分
+
+        Returns:
+            gx: 入力側に伝わる微分
+        """
+        x = self.inputs[0]
+        gx = gy * -sin(x)
+        return gx
+
+
+def cos(x: Variable) -> Variable:
+    """cos関数
+
+    Args:
+        x: 入力
+
+    Returns:
+        y: 出力
+    """
+    return Cos()(x)
---
+++ steps/step34.py
@@ -0,0 +1,25 @@
+# 34. sin関数の高階微分
+import matplotlib.pyplot as plt
+import numpy as np
+
+import dezero.functions as F
+from dezero import Variable
+
+if __name__ == "__main__":
+    x = Variable(np.linspace(-7, 7, 200))
+    y = F.sin(x)
+    y.backward(create_graph=True)
+
+    logs = [y.data]
+
+    for i in range(3):
+        logs.append(x.grad.data)
+        gx = x.grad
+        x.cleargrad()
+        gx.backward(create_graph=True)
+
+    labels = ["y=sin(x)", "y'", "y''", "y'''"]
+    for i, v in enumerate(logs):
+        plt.plot(x.data, logs[i], label=labels[i])
+    plt.legend(loc="lower right")
+    plt.show()
