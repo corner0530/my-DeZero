2efc8d9c07a65ff0046643a3f185632e73c522fa
corner0530 <2531yasuaki@gmail.com>
2023-04-06 21:42:53 +0900

    Done step32

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,6 +1,18 @@
 # simple version
 
-from dezero.core_simple import (
+# from dezero.core_simple import (
+#     Function,
+#     Variable,
+#     as_array,
+#     as_variable,
+#     no_grad,
+#     setup_variable,
+#     using_config,
+# )
+
+# full version
+
+from dezero.core import (
     Function,
     Variable,
     as_array,
@@ -9,28 +21,5 @@ from dezero.core_simple import (
     setup_variable,
     using_config,
 )
-
-# full version
-
-# import dezero.cuda
-# import dezero.dataset
-# import dezero.datasets
-# import dezero.functions
-# import dezero.functions_conv
-# import dezero.layers
-# import dezero.optimizers
-# import dezero.utils
-# from dezero.core import (
-#     Function,
-#     Parameter,
-#     Variable,
-#     as_variable,
-#     config,
-#     no_grad,
-#     setup_variable,
-#     test_mode,
-#     using_config,
-# )
-# from dezero.layers import Layer, Model
 
 setup_variable()
